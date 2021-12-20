4f98887b5d9ac3b1728cd35a2ebba67866693b33
Kaspar Poland <kaspar78@mouco.com>
2021-12-19 19:15:00 -0700

    Add "meet boos"

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,17 @@ function App() {
       justifyContent="center"
       alignItems="center"
     >
+      <Grid item container direction="row" justifyContent="flex-start">
+        <p
+          style={{
+            fontSize: "3rem",
+            lineHeight: "0.5rem",
+            paddingLeft: "3rem",
+          }}
+        >
+          {emoji.getUnicode("shrimp")}
+        </p>
+      </Grid>
       <Section>
         <h1>
           Meet the <s>founders</s> shrimp
@@ -32,6 +43,30 @@ function App() {
         <h1>
           {trainEmoji}boos{trainEmoji}
         </h1>
+      </Section>
+      <Section>
+        <p style={{ fontSize: "1.5rem" }}>
+          The team here at <strong>6krill&copy;&trade;&reg;</strong> is
+          dedicated to making cash and getting earning. Helping our clients make
+          money since 1900, we really bring in the big bucks. There is nothing
+          that can block the path between a shrimp and the dough.
+        </p>
+
+        <p>
+          Meet boos. boos is our biggest hitter. As the inventor of the 6krill
+          title, boos got ideas. As a Dust II hater, a young producer, and gamer
+          on the sticks, boos really stays odd around the hose. boos is valuable
+          to 6krill in his sheer unconformity. boos the type of guy to order a
+          whole plate of french toast and ask for a box at the same time. boos
+          keep it weird.
+        </p>
+
+        <p>Meet 2nuts.</p>
+        {/* <p>
+          Getting money isn't the only thing the shrimp do. With recent
+          technological advances in the fields of neuroscience and flatnite,
+          6krill provides a frictionless solution to every problem. Whether it be "too much wind power"
+        </p> */}
       </Section>
     </Grid>
   );
