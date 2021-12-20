2f5f9f8792909607eca50762565e06b549cad870
luke <luke@lukeaskew.xyz>
2021-12-19 19:15:26 -0700

    added some ideas and such

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Section from "./Section";
 import Grid from "@material-ui/core/Grid";
 import emoji from "emoji-dictionary";
+import Button from "@material-ui/core/Button"
+
 
 function App() {
   const cakeEmoji = emoji.getUnicode("birthday");
@@ -33,7 +35,26 @@ function App() {
           {trainEmoji}boos{trainEmoji}
         </h1>
       </Section>
+
+
+
+      <div>
+        <h1> How we do it </h1>
+        <Button variant = "outlined" className="btn btn-dark">PRESS RELEASE</Button>
+        <p>
+        At wyayouat.com, we are committed to utilizing the power of blockchain technology.
+        With advanced machine learning algorithms harnessing ECC, TDA, ECC, bio-machine interfaces, cybernetics, post quantum cryptography we make the most sustainable product on the planet. Our decentralized cryptosystems and blockchains are international,
+         and we share in the needs of our multlingual conglomorate allies in standing against global.
+
+        We also sell used socks. DM me for pricing. Steroids optional.
+        </p>
+      </div>
     </Grid>
+
+
+
+
+
   );
 }
 
