1b733ca6e2da609970136dceeb860816b4b2890b
Kaspar Poland <kaspar78@mouco.com>
2021-12-19 20:44:06 -0700

    ballmer peak

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from "react";
-import Section from "./Section";
 import Grid from "@material-ui/core/Grid";
-import emoji from "emoji-dictionary";
-import Button from "@material-ui/core/Button"
+import Button from "@material-ui/core/Button";
 
+import Card from "./Card";
+import Section from "./Section";
+
+import emoji from "emoji-dictionary";
+
+const founders = {
+  cake: {
+    name: "cake",
+    blurb: "something",
+  },
+  "2nuts": {
+    name: "2nuts",
+    blurb: "something else",
+  },
+  boos: {
+    name: "boos",
+    blurb: `Meet boos. boos is our biggest hitter. As the inventor of the 6krill
+    title, boos got ideas. As a Dust II hater, a young producer, and
+    gamer on the sticks, boos really stays odd around the hose. boos is
+    valuable to 6krill in his sheer unconformity. boos the type of guy
+    to order a whole plate of french toast and ask for a box at the same
+    time. boos keep it weird.`,
+  },
+};
 
 function App() {
   const cakeEmoji = emoji.getUnicode("birthday");
@@ -16,81 +38,64 @@ function App() {
       direction="column"
       justifyContent="center"
       alignItems="center"
+      style={{
+        paddingTop: "20px",
+      }}
     >
-      <Grid item container direction="row" justifyContent="flex-start">
-        <p
-          style={{
-            fontSize: "3rem",
-            lineHeight: "0.5rem",
-            paddingLeft: "3rem",
-          }}
-        >
-          {emoji.getUnicode("shrimp")}
-        </p>
-      </Grid>
-      <Section>
-        <h1>
-          Meet the <s>founders</s> shrimp
-        </h1>
-      </Section>
-      <Section>
-        <h1>
-          {cakeEmoji}
-          cake
-          {cakeEmoji}
-        </h1>
-        <h1>
-          {nutEmoji}2nuts{nutEmoji}
-        </h1>
-        <h1>
-          {trainEmoji}boos{trainEmoji}
-        </h1>
-      </Section>
-
-
-
-
-      <Section>
-        <p style={{ fontSize: "1.5rem" }}>
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        xs={8}
+      >
+        <Section>
+          <h1>
+            {emoji.getUnicode("shrimp")}
+            Meet the <s>founders</s> shrimp
+            {emoji.getUnicode("shrimp")}
+          </h1>
+        </Section>
+        <Section>
+          <h1>
+            {cakeEmoji}
+            cake
+            {cakeEmoji}
+          </h1>
+          <h1>
+            {nutEmoji}2nuts{nutEmoji}
+          </h1>
+          <h1>
+            {trainEmoji}boos{trainEmoji}
+          </h1>
+        </Section>
+        <Card alignLeft={true} title="About us" founder={founders.cake}>
           The team here at <strong>6krill&copy;&trade;&reg;</strong> is
           dedicated to making cash and getting earning. Helping our clients make
           money since 1900, we really bring in the big bucks. There is nothing
           that can block the path between a shrimp and the dough.
-        </p>
-
-        <p>
-          Meet boos. boos is our biggest hitter. As the inventor of the 6krill
-          title, boos got ideas. As a Dust II hater, a young producer, and gamer
-          on the sticks, boos really stays odd around the hose. boos is valuable
-          to 6krill in his sheer unconformity. boos the type of guy to order a
-          whole plate of french toast and ask for a box at the same time. boos
-          keep it weird.
-        </p>
-
-        <p>Meet 2nuts.</p>
-        {/* <p>
-          Getting money isn't the only thing the shrimp do. With recent
-          technological advances in the fields of neuroscience and flatnite,
-          6krill provides a frictionless solution to every problem. Whether it be "too much wind power"
-        </p> */}
-        <div>
-          <h1> How we do it </h1>
-          <Button variant = "outlined" className="btn btn-dark">PRESS RELEASE</Button>
-          <p>
-          At wyayouat.com, we are committed to utilizing the power of blockchain technology.
-          With advanced machine learning algorithms harnessing ECC, TDA, ECC, bio-machine interfaces, cybernetics, post quantum cryptography we make the most sustainable product on the planet. Our decentralized cryptosystems and blockchains are international,
-           and we share in the needs of our multlingual conglomorate allies in standing against global.
-
-          We also sell used socks. DM me for pricing. Steroids optional.
-          </p>
-        </div>
-      </Section>
+        </Card>
+        <br />
+        <Card
+          alignLeft={false}
+          title="How we do it"
+          founder={founders["2nuts"]}
+        >
+          At wyayouat.com, we are committed to utilizing the power of blockchain
+          technology. With advanced machine learning algorithms harnessing ECC,
+          TDA, ECC, bio-machine interfaces, cybernetics, post quantum
+          cryptography we make the most sustainable product on the planet. Our
+          decentralized cryptosystems and blockchains are international, and we
+          share in the needs of our multilingual conglomerate allies in standing
+          against global. We also sell used socks. DM me for pricing. Steroids
+          optional.
+        </Card>
+        <br />
+        <Card alignLeft={true} title="Credit Card" founder={founders.boos}>
+          CREDIT CARD
+        </Card>
+      </Grid>
     </Grid>
-
-
-
-
-
   );
 }
 
---
+++ src/Card.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { Paper } from "@material-ui/core";
+
+interface Founder {
+  name: string;
+  blurb: string;
+}
+
+function Card({
+  alignLeft,
+  founder,
+  title,
+  children,
+}: {
+  alignLeft: boolean;
+  founder: Founder;
+  title: string;
+  children: React.ReactNode | React.ReactNode[];
+}) {
+  const whichBorder = alignLeft ? "borderRight" : "borderLeft";
+  const whichMargin = alignLeft ? "marginRight" : "marginLeft";
+
+  const founderSection = (
+    <Grid
+      container
+      direction="column"
+      xs={4}
+      style={{
+        [whichBorder]: "2px solid gray",
+        [whichMargin]: "10px",
+        padding: "10px",
+      }}
+    >
+      <h2>{founder.name}</h2>
+      <div>{founder.blurb}</div>
+    </Grid>
+  );
+
+  const textSection = (
+    <Grid xs={7} direction="column" style={{ paddingBottom: "20px" }}>
+      <h2>{title}</h2>
+      {children}
+    </Grid>
+  );
+
+  return (
+    <Paper style={{ padding: "20px" }}>
+      <Grid container direction="row">
+        {alignLeft ? (
+          <React.Fragment>
+            {founderSection} {textSection}
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            {textSection} {founderSection}
+          </React.Fragment>
+        )}
+      </Grid>
+    </Paper>
+  );
+}
+
+export default Card;
